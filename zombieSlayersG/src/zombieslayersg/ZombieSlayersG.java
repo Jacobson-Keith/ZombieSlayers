1f999c938a40663f094074c1671f6063d95d2437
Chicho <Chicho@192.168.0.47>
2017-02-17 08:22:33 -0300

    1/28/2017 - Creation of classes, functions, and test cases for the View Layer

--- zombieSlayersG/src/zombieslayersg/ZombieSlayersG.java
+++ zombieSlayersG/src/zombieslayersg/ZombieSlayersG.java
@@ -20,6 +20,7 @@ import buyi.cit260.zombieSlayers.model.Inventory;
 import buyi.cit260.zombieSlayers.model.Item;
 import buyi.cit260.zombieSlayers.model.Room;
 import buyi.cit260.zombieSlayers.model.Treasure;
+import byui.cit260.zombieSlayers.view.GameStartZS;
 
 
 /**
@@ -33,97 +34,10 @@ public class ZombieSlayersG {
      */
     public static void main(String[] args) {
         
-       Player playerOne = new Player();
-              
-       playerOne.setName("Chicho");
-       playerOne.setHp(10);
-       playerOne.setStr(1);
-       
-       String playerInfo = playerOne.toString();
-       System.out.println(playerInfo);
-       
-       //Here we can create instances of the classes being developed
-       Potion rPotion = new Potion();
-       Food ham = new Food();
-       Zombie weakZombie = new Zombie();
-       Door woodDoor = new Door();
-       Map basement3 = new Map();
-       //Here are the five created by Keith.
-       Weapon bastardSword = new Weapon();
-       Inventory contents  = new Inventory();
-       Item key = new Item();
-       Room hallWay = new Room();
-       Treasure plainChest = new Treasure();
-       
-       // Here we can add values to the attributes on those objects calling
-       // for their setters functions
-       
-       rPotion.setType("Red Potion");
-       rPotion.setFunction("Restores +5 HP");
-       
-       ham.setName("Ham");
-       ham.setAbility("Restores +1 HP per room for 3 rooms");
-       
-       weakZombie.setType("Weak Zombie");
-       weakZombie.setHp(4);
-       weakZombie.setStr(1);
-       weakZombie.setDamage(1);
-       
-       woodDoor.setSturdiness(2);
-       
-       basement3.setRowCount(1);
-       basement3.setColumnCount(1);
-       
-       bastardSword.setType("Melee");
-       bastardSword.setBaseAttack(3);
-       bastardSword.setReqStr(2);
-       
-       contents.setShowContents("The following items are in you inventory: ");
-       
-       key.setName("Key");
-       key.setType("Bronze");
-       key.setFunction("Use key to unlock a bronze door to get out of 3rd basement.");
-       
-       hallWay.setName("Hall Way");
-       hallWay.setMapLocation(7);
-       hallWay.setDesc("You enter a long hallway that is dimly lit, you can hear moaning coming from the door on the left. A locked door is in front of you, and a slightly ajar door is to your right.");
-       
-       plainChest.setLock("No");
-       plainChest.setSturdiness(1);
-       plainChest.setTreasureLvl(1);
-       
-       // And here we'll print them out to the System, so that we can check
-       // everything worked out as planned 
-       
-       String rPotionInfo = rPotion.toString();
-       System.out.println(rPotionInfo);
-       
-       String hamInfo = ham.toString();
-       System.out.println(hamInfo);
-       
-       String weakZombieInfo = weakZombie.toString();
-       System.out.println(weakZombieInfo);
-       
-       String woodDoorInfo = woodDoor.toString();
-       System.out.println(woodDoorInfo);
-       
-       String basement3Info = basement3.toString();
-       System.out.println(basement3Info);
-       
-       String bastardSwordInfo = bastardSword.toString();
-       System.out.println(bastardSwordInfo);
-       
-       String contentsInfo = contents.toString();
-       System.out.println(contentsInfo);
-       
-       String keyInfo = key.toString();
-       System.out.println(keyInfo);
-       
-       String hallWayInfo = hallWay.toString();
-       System.out.println(hallWayInfo);
-       
-       String plainChestInfo = plainChest.toString();
-       System.out.println(plainChestInfo);
+        GameStartZS gameStart = new GameStartZS();
+        gameStart.displayBanner();
+        gameStart.displayMenuView();
+        
     }
     
 }
