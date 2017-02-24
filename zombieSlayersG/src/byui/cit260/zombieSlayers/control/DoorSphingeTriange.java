c94a843bdda683c5cf79bfc49f92d303a2d4220b
Keith Jacobson <JacobsonKe@0103075FXA.edmonds.wednet.edu>
2017-02-23 22:46:18 -0800

    Revert "2/21/2017 - It looks like I am confirming the deleting of the Test Package? I want to test this out before I do anything else."
    This reverts commit dd349fa880d2904dd29fb6e207132c3b7510bb8e.

---
+++ zombieSlayersG/src/byui/cit260/zombieSlayers/control/DoorSphingeTriange.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package byui.cit260.zombieSlayers.control;
+
+/**
+ *
+ * @author Keith
+ */
+public class DoorSphingeTriange {
+    
+    public int calcTriArea(int sideOne, int sideTwo){
+        
+        int totalArea = ((sideOne * sideTwo) / 2);
+        
+        return totalArea;
+    }
+}
