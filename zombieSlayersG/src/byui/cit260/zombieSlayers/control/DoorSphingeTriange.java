10b47a363d7e48575cdb89677b54dad7e052a3ac
Keith Jacobson <>
2017-02-23 22:54:09 -0800

    Revert "2/21/2017 - Seeing what happens if I save a modified deleted file...
    This reverts commit dd349fa880d2904dd29fb6e207132c3b7510bb8e.

--- zombieSlayersG/src/byui/cit260/zombieSlayers/control/DoorSphingeTriange.java
+++ zombieSlayersG/src/byui/cit260/zombieSlayers/control/DoorSphingeTriange.java
@@ -17,4 +17,26 @@ public class DoorSphingeTriange {
         
         return totalArea;
     }
+
+public String validateAnswers (int totalArea){
+
+        String perfect = "That's!... You!... Just!... OH ALLRIGHT! YOU GOT IT PERFECTLY... You cheater... YOU LOOKED INTO THE CODE, DIDNT YOU?!";
+        String correct = "Well... That's not as accurate as it could be, but it'll do, I guess... Humph...";
+        String wrongSmall = "HA! That's not even close! My area is bigger than that!";
+        String wrongBig = "HA! I WISH I'D BE THAT BIG! I am smaller than that though...";
+        
+        
+        if (totalArea == 112){
+            return perfect;
+        }
+        else if (totalArea < 68){
+            return wrongSmall;
+        }
+        else if (totalArea > 214){
+            return wrongBig;
+        }
+        else {
+            return correct;
+        }
+    }
 }
